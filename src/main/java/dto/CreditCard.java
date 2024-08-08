b447527eb21fd2e8aeab928e79d4b29a28593412
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2024-08-08 12:32:59 +0300

    add CreditCard Entity and Initial commit in feature/major-release

---
+++ src/main/java/dto/CreditCard.java
@@ -0,0 +1,8 @@
+package main.java.dto;
+
+public class CreditCard {
+    private String cardNumber;
+    private String cardHolderName;
+    private String expirationDate;
+    private String cvv;
+}
