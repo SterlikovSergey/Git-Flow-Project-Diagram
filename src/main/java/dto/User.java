620d5acc773de98147e2e4217cf4a50120f19dc5
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2024-08-08 14:40:35 +0300

    Start of release branch for 1.0

--- src/main/java/dto/User.java
+++ src/main/java/dto/User.java
@@ -1,7 +1,10 @@
 package main.java.dto;
 
+import java.util.List;
+
 public class User extends UserDetails{
     private String name;
     private String email;
+    private List<Phone> phones;
     private CreditCard creditCard;
 }
