0254e0485f324d759d61c06fa52487813814d437
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2024-08-08 16:30:59 +0300

    add Email Entity in User entity

--- src/main/java/dto/User.java
+++ src/main/java/dto/User.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class User extends UserDetails{
     private String name;
-    private String email;
     private List<Phone> phones;
+    private List<Email> emails;
     private CreditCard creditCard;
     private Order order;
 }
